ff1ddfbb603a2b372ff526e1fc6bd31979e4cac2
bengisu1 <138867433+bengisu1@users.noreply.github.com>
2024-05-25 22:47:38 +0300

    Add files via upload

---
+++ index_file.js
@@ -0,0 +1,30 @@
+function greetUser() {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = 'Hello! Welcome to our project!';
+}
+
+function openPdf(pdfUrl) {
+    window.open(pdfUrl, '_blank');
+}
+
+function addVideos() {
+    const videoElement = document.getElementById('video');
+    const videoSetting = new Video();
+    videoElement.textContent = `Added videos: ${videoSetting.toLocaleVideoString()}`;
+}
+
+function openVideoProcessing() {
+    
+}
+
+function openImageProcessing() {
+    
+}
+
+function openLiveProcessing() {
+    
+}
+
+function openSettings() {
+    
+}
